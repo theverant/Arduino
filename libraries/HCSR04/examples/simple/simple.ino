879a4be01b3dcac40e2dd043461dc785a8702bdd
NSCADNathan <64323790+NSCADNathan@users.noreply.github.com>
2022-03-13 21:14:42 -0300

    name change

--- libraries/HCSR04/examples/simple/simple.ino
+++ libraries/HCSR04/examples/simple/simple.ino
@@ -1,6 +1,6 @@
 #include <HCSR04.h>
 
-UltraSonicDistanceSensor distanceSensor(13, 12);  // Initialize sensor that uses digital pins 13 and 12.
+UltraSonicDistanceSensor distanceSensor(14, 15);  // Initialize sensor that uses digital pins 13 and 12.
 
 void setup () {
     Serial.begin(9600);  // We initialize serial connection so that we could print values from sensor.
